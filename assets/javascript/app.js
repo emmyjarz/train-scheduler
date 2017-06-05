0c2970b7ba5627a9d5309f9b9e8de24659a44ed1
Thanita Emmy Jarzebinski <emmyjarz@gmail.com>
2017-06-05 14:47:54 -0700

    html

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+$("button").on("click", function(event){
+  event.preventDefault();
+  var name = $("#name").val().trim();
+  var destination = $("#destination").val().trim();
+  var time = $("#time").val().trim();
+  var freq = $("#freq").val().trim();
+
+});
